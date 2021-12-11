fbcaef9907dcbd6e3067dd32029c47d612588139
Vinh <ladykillah962@gmail.com>
2021-12-11 10:46:41 +0700

    update regex input

--- TSP/frmHelp.Designer.cs
+++ TSP/frmHelp.Designer.cs
@@ -28,24 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHelp));
-            this.SuspendLayout();
-            // 
-            // frmHelp
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(485, 470);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "frmHelp";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Help";
-            this.ResumeLayout(false);
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHelp));
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(13, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(668, 40);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Ứng dụng tìm đường đi ngắn nhất từ một đảo đến các đảo còn lại (mỗi đảo chỉ đến m" +
+    "ột lần) và quay trở về đảo ban đầu.\r\n";
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(13, 62);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(668, 22);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Trước hết, người dùng cần nhập thông tin các đảo vào ô textbox theo đúng quy định" +
+    " sau:\r\n";
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(33, 84);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(648, 46);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "- Mỗi dòng chứa thông tin của một đảo bao gồm: tọa độ x, tọa độ y, tên đảo. Những" +
+    " thông tin này phải được nhập theo đúng thứ tự và cách nhau bằng dấu phẩy (\",\")." +
+    "\r\n";
+			// 
+			// frmHelp
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoScroll = true;
+			this.ClientSize = new System.Drawing.Size(693, 532);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "frmHelp";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Hướng dẫn sử dụng";
+			this.ResumeLayout(false);
 
         }
 
-        #endregion
-    }
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+	}
 }
--- TSP/frmHome.cs
+++ TSP/frmHome.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Globalization;
 using System.IO;
@@ -20,7 +21,8 @@ namespace TSP
             InitializeComponent();
         }
 
-        List<PointF> points = new List<PointF>();
+		private const string V = @"C:\your.xlsx";
+		List<PointF> points = new List<PointF>();
 
 		List<PointF> lines = new List<PointF>();
 
@@ -41,53 +43,48 @@ namespace TSP
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
+			info = txtInfo.Text.Trim();
+
+			if (info.Length == 0)
+			{
+				handleInput("Vui lòng nhập thông tin!", 
+					"Thông tin không được bỏ trống",
+					MessageBoxButtons.OK, MessageBoxIcon.Warning, txtInfo);
+				return;
+			}
+			for (int index = 0; index < txtInfo.Lines.Length; index++) {
+				string[] line = txtInfo.Lines[index].Trim().Split(new char[] { ',' },
+					StringSplitOptions.RemoveEmptyEntries);
+				if (line.Length == 0 || line == null) {
+					continue;
+				} else if (line.Length > 3 || !isNumber(line[0]) || !isNumber(line[1])) {
+					handleInput("Vui lòng xem lại hướng dẫn và nhập lại!", 
+						"Thông tin không hợp lệ",
+						MessageBoxButtons.OK, MessageBoxIcon.Warning, txtInfo);
+					return;
+				}
+			}
+			using (StreamWriter file = new StreamWriter(filePath))
+			{
+				file.WriteLine(info);
+				//file.Close();
+			}
+			showMessage("Thêm thông tin thành công!!!", "Thông  báo",
+				MessageBoxButtons.OK, MessageBoxIcon.Information);
 			clearData();
-            cbxStart.Items.Add("--Chọn đảo bắt đầu--");
-            
-            if (txtInfo.Text.Trim().Length == 0)
-            {
-                handleInput("Vui lòng nhập thông tin!!!", "Warning",
-                    MessageBoxButtons.OK, MessageBoxIcon.Warning, txtInfo);
-                return;
-            }
-            else
-            {
-                string[] points = txtInfo.Text.Trim().Split(new[] { Environment.NewLine },
-                    StringSplitOptions.None);
-                foreach (string point in points)
-                {
-                    string[] part = point.Trim().Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries);
-                    if (!isNumber(part[0].Trim()) || !isNumber(part[1].Trim()))
-                    {
-                        handleInput("Tọa độ đảo phải là số!!!", "Dữ liệu không hợp lệ",
-                            MessageBoxButtons.OK, MessageBoxIcon.Error, txtInfo);
-                        return;
-                    }
-                    cbxStart.Items.Add(part[2].Trim());
-                    names.Add(part[2].Trim());
-                }
-            }
-            info = txtInfo.Text.Trim();
-            using (StreamWriter file = new StreamWriter(filePath))
-            {
-                file.WriteLine(info);
-                file.Close();
-            }
-            showMessage("Thêm thông tin thành công!!!", "Success",
-                MessageBoxButtons.OK, MessageBoxIcon.Information);
-            cbxStart.SelectedIndex = 0;
-            //selectedIndex = -100;
-            btnDel.Enabled = true;
-            isPaint = true;
-            pnlResult.Controls.Clear();
-            pnlResult.Refresh();
+			cbxStart.Items.Add("--Chọn đảo bắt đầu--");
+			cbxStart.SelectedIndex = 0;
+			btnDel.Enabled = true;
+			isPaint = true;
+			pnlResult.Controls.Clear();
+			pnlResult.Refresh();
         }
         
         private void btnDel_Click(object sender, EventArgs e)
         {
             clearData();
-            txtInfo.Text = "";
-            btnDel.Enabled = false;
+			btnDel.Enabled = false;
+			txtInfo.Text = "";
             isPaint = false;
             cbxStart.Text = "";
             cbxStart.Items.Add("--Chọn đảo bắt đầu--");
@@ -98,6 +95,7 @@ namespace TSP
 
         void clearData()
         {
+			btnSave.Enabled = false;
 			isSuccess = false;
             row = 0;
 			selectedIndex = -100;
@@ -110,22 +108,30 @@ namespace TSP
 
         private void btnFind_Click(object sender, EventArgs e)
 		{
-			float total = 0, lowerBound = 0, minTempResult = 999999999;
+			if (amount > 2)
+			{
+				float total = 0, lowerBound = 0, minTempResult = 999999999;
 
-			edges = new Edge[amount, amount];
-			results = new Edge[amount];
-			tempResults = new Edge[amount];
+				edges = new Edge[amount, amount];
+				results = new Edge[amount];
+				tempResults = new Edge[amount];
 
-			fillEdge(edges, amount);
+				fillEdge(edges, amount);
 
-			tsp(edges, results, tempResults, ref total,
-                ref lowerBound, ref minTempResult, amount, 0, startPoint);
-			showMessage("Tìm kiếm thành công!", "Success",
-					MessageBoxButtons.OK, MessageBoxIcon.Information);
-			displayResult(results, amount);
+				tsp(edges, results, tempResults, ref total,
+					ref lowerBound, ref minTempResult, amount, 0, startPoint);
+				showMessage("Tìm kiếm thành công!", "Thông báo",
+						MessageBoxButtons.OK, MessageBoxIcon.Information);
+				displayResult(results, amount);
 
-			isSuccess = true;
-			this.pnlResult.Refresh();
+				isSuccess = true;
+				this.pnlResult.Refresh();
+				btnSave.Enabled = true;
+			}
+			else {
+				showMessage("Tìm kiếm thất bại!\nSố đảo phải lớn hơn 2!", 
+					"Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
 		}
 
         private void btnHelp_Click(object sender, EventArgs e)
@@ -134,19 +140,11 @@ namespace TSP
             frm.ShowDialog();
         }
 
-        bool isNumber(string str, string type = "number")
+        bool isNumber(string str)
         {
-            Regex regex = new Regex("");
             bool check = false;
-            switch (type)
-            {
-                case "integer":
-                    regex = new Regex("^[0-9]+$");
-                    break;
-                default:
-                    regex = new Regex(@"^-?[0-9]+[0-9]*(\.[0-9]+)?$");
-                    break;
-            }
+            Regex regex = new Regex(@"^-?[0-9]+[0-9]*(\.[0-9]+)?$");
+            
             if (regex.IsMatch(str))
             {
                 check = true;
@@ -190,73 +188,92 @@ namespace TSP
 
         private void pnlResult_Paint(object sender, PaintEventArgs e)
         {
-             
-            if (isPaint)
-            {
-				points.Clear();
-				lines.Clear();
-				row = 0;
+			try
+			{
 
-				StreamReader file = new StreamReader(filePath);
-                Brush color;
-                Pen pinkPen = new Pen(Color.DeepPink, 3);
-                string line;
-                float round = scale;
-                float xCoordinate;
-                float yCoordinate;
-                float xOrigin = (pnlResult.Width / 2) - (round / 2);
-                float yOrigin = (pnlResult.Height / 2) - (round / 2);
-
-                while ((line = file.ReadLine()) != null)
-                {
-                    row++;
-                    if (row == selectedIndex)
-                    {
-                        color = Brushes.Orange;
-                    }
-                    else
-                    {
-                        color = Brushes.Black;
-                    }
-                    string[] part = line.Trim().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-                    xCoordinate = (float.Parse(part[0], CultureInfo.InvariantCulture.NumberFormat) 
-						* scale + xOrigin);
-                    yCoordinate = (float.Parse(part[1], CultureInfo.InvariantCulture.NumberFormat) 
-						* scale * -1 + yOrigin);
-					lines.Add(new PointF(xCoordinate, yCoordinate));
-
-					points.Add(new PointF(float.Parse(part[0], CultureInfo.InvariantCulture.NumberFormat),
-						float.Parse(part[1], CultureInfo.InvariantCulture.NumberFormat)));
-                    e.Graphics.FillEllipse(color, xCoordinate, yCoordinate, round, round);
-					
-					Label name = new Label();
-                    name.Text = part[2];
-                    name.Location = new Point((int)(xCoordinate + scale/2) , (int)(yCoordinate - scale/2));
-                    name.ForeColor = Color.White;
-                    name.BackColor = Color.Transparent;
-                    name.AutoSize = true;
-                    name.Padding = new Padding(0);
-                    pnlResult.Controls.Add(name);
-                }
-
-				startPoint = selectedIndex - 1;
-                amount = points.Count;
-
-				file.Close();
-
-				//draw line
-				if (isSuccess)
+				if (isPaint)
 				{
-					for (int i = 0; i < amount; i++)
-					{
-						Pen greenPen = new Pen(Color.Green, lineWidth);
-						e.Graphics.DrawLine(greenPen, lines[results[i].getStartPoint()],
-							lines[results[i].getEndPoint()]);
+					points.Clear();
+					lines.Clear();
+					row = 0;
 
-						
+					StreamReader file = new StreamReader(filePath);
+					Brush color;
+					Pen pinkPen = new Pen(Color.DeepPink, 3);
+					string line;
+					float round = scale;
+					float xCoordinate;
+					float yCoordinate;
+					float xOrigin = (pnlResult.Width / 2) - (round / 2);
+					float yOrigin = (pnlResult.Height / 2) - (round / 2);
+
+					while ((line = file.ReadLine()) != null)
+					{
+						row++;
+						if (row == selectedIndex)
+						{
+							color = Brushes.Orange;
+						}
+						else
+						{
+							color = Brushes.Black;
+						}
+						string[] part = line.Trim().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+						if (part == null || part.Length == 0)
+						{
+							continue;
+						}
+						else
+						{
+							xCoordinate = (float.Parse(part[0], CultureInfo.InvariantCulture.NumberFormat)
+							* scale + xOrigin);
+							yCoordinate = (float.Parse(part[1], CultureInfo.InvariantCulture.NumberFormat)
+								* scale * -1 + yOrigin);
+							lines.Add(new PointF(xCoordinate, yCoordinate));
+
+							points.Add(new PointF(float.Parse(part[0], CultureInfo.InvariantCulture.NumberFormat),
+								float.Parse(part[1], CultureInfo.InvariantCulture.NumberFormat)));
+							e.Graphics.FillEllipse(color, xCoordinate, yCoordinate, round, round);
+
+							if (hasExistItem(cbxStart, (part[2].Trim())))
+							{
+								cbxStart.Items.Add(part[2].Trim());
+							}
+							names.Add(part[2].Trim());
+
+							Label name = new Label();
+							name.Text = part[2];
+							name.Location = new Point((int)(xCoordinate + scale / 2), (int)(yCoordinate - scale / 2));
+							name.ForeColor = Color.White;
+							name.BackColor = Color.Transparent;
+							name.AutoSize = true;
+							name.Padding = new Padding(0);
+							pnlResult.Controls.Add(name);
+						}
 					}
 
-				}				
+					startPoint = selectedIndex - 1;
+					amount = points.Count;
+
+					file.Close();
+
+					//draw line
+					if (isSuccess)
+					{
+						for (int i = 0; i < amount; i++)
+						{
+							Pen pen = new Pen(Color.DeepPink, lineWidth);
+							e.Graphics.DrawLine(pen, lines[results[i].getStartPoint()],
+								lines[results[i].getEndPoint()]);
+						}
+
+					}
+				}
+			}
+			catch (Exception ex) {
+				string content = "Vui lòng kiểm tra lại thông tin các đảo và thử lại!";
+				showMessage(content, "Có lỗi xảy ra", MessageBoxButtons.OK,
+					MessageBoxIcon.Error);
 			}
         }
         
@@ -315,9 +332,8 @@ namespace TSP
             }
             lblResult.Text += "\nTổng khoảng cách = " + sum + " hải lý";
         }
-
-        //Branch and Bound for TSP
-        float findMinEdge(Edge[, ] edges, int amount)
+		
+		float findMinEdge(Edge[, ] edges, int amount)
         {
             float min = 999999999;
             for (int i = 0; i < amount; i++)
@@ -364,32 +380,45 @@ namespace TSP
         void tsp(Edge[, ] edges, Edge[] results, Edge[] tempResults, ref float total,
             ref float lowerBound, ref float minTempResult, int amount, int rank, int startPoint)
         {
-            
-            for(int nextPoint = 0; nextPoint < amount; nextPoint++)
-                if(startPoint != nextPoint && 
-                    !edges[startPoint, nextPoint].getIsVisited() && 
-                    !hasCycle(tempResults, rank, nextPoint))
-                {
+			for (int nextPoint = 0; nextPoint < amount; nextPoint++)
+			{
+				if (startPoint != nextPoint &&
+					!edges[startPoint, nextPoint].getIsVisited() &&
+					!hasCycle(tempResults, rank, nextPoint))
+				{
 
-                    edges[startPoint, nextPoint].setIsVisited(true);
-                    edges[nextPoint, startPoint].setIsVisited(true);
-                    total += edges[startPoint, nextPoint].getLength();
-                    lowerBound = findLowerBound(edges, total, amount, rank+1);
-                    if(lowerBound < minTempResult)
-                    {
-                        tempResults[rank] = edges[startPoint, nextPoint].Clone();
-                        if (rank == amount - 2)
-                            updateTemporaryResult(edges, results, tempResults, total, 
-                                ref minTempResult, amount);
-                        else
-                            tsp(edges, results, tempResults, ref total, ref lowerBound,
-                                ref minTempResult, amount, rank+1, nextPoint);
-                    }
-                    total -= edges[startPoint, nextPoint].getLength();
-                    edges[startPoint, nextPoint].setIsVisited(false);
-                    edges[nextPoint, startPoint].setIsVisited(false);
-                }
-        }
+					edges[startPoint, nextPoint].setIsVisited(true);
+					edges[nextPoint, startPoint].setIsVisited(true);
+					total += edges[startPoint, nextPoint].getLength();
+					lowerBound = findLowerBound(edges, total, amount, rank + 1);
+					if (lowerBound < minTempResult)
+					{
+						tempResults[rank] = edges[startPoint, nextPoint].Clone();
+						if (rank == amount - 2)
+							updateTemporaryResult(edges, results, tempResults, total,
+								ref minTempResult, amount);
+						else
+							tsp(edges, results, tempResults, ref total, ref lowerBound,
+								ref minTempResult, amount, rank + 1, nextPoint);
+					}
+					total -= edges[startPoint, nextPoint].getLength();
+					edges[startPoint, nextPoint].setIsVisited(false);
+					edges[nextPoint, startPoint].setIsVisited(false);
+				}
+			}
+		}
+
+		bool hasExistItem(ComboBox cbx, string item) {
+			return cbx.FindString(item) == -1;
+		}
+		// copy point
+		List<PointF> copyPoints(List<PointF> currentPoints) {
+			List<PointF> tempPoints = new List<PointF>();
+			for (int index = 0; index < currentPoints.Count; index++) {
+				tempPoints[index] = currentPoints[index];
+			}
+			return tempPoints;
+		}
 
 		private void numericUpDownScale_ValueChanged(object sender, EventArgs e)
         {
@@ -398,5 +427,36 @@ namespace TSP
             this.pnlResult.Controls.Clear();
             this.pnlResult.Refresh();
         }
-    }
+
+		private void btnSave_Click(object sender, EventArgs e)
+		{
+			SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+			saveFileDialog.Filter = "Text File | *.txt";
+			saveFileDialog.FilterIndex = 2;
+			saveFileDialog.RestoreDirectory = true;
+			saveFileDialog.FileName = "result.txt";
+			saveFileDialog.Title = "Lưu kết quả tìm kiếm";
+
+			if (saveFileDialog.ShowDialog() == DialogResult.OK)
+			{
+				using (StreamWriter file = new StreamWriter(saveFileDialog.OpenFile()))
+				{
+					file.WriteLine(lblResult.Text);
+					file.Close();
+				}
+
+				string content = "Lưu kết quả tìm kiếm thành công!" +
+					"\nBạn có muốn mở file " + saveFileDialog.FileName + " không?";
+
+
+				DialogResult dialogResult = MessageBox.Show(content, "Thông báo",
+					MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+				if (dialogResult == DialogResult.Yes)
+				{
+					Process.Start(saveFileDialog.FileName);
+				}
+			}
+		}
+	}
 }
